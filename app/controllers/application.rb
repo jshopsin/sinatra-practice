7ded90f937fa2be32d89ef1e67fe0f1c5ad1a371
Jennifer Shopsin <jshopsin@yahoo.com>
2017-08-07 22:34:27 -0700

    Add routes to controller

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -1,3 +1,52 @@
 get '/' do
   "Hello World"
 end
+
+# CONTACTS RESTful actions
+
+
+# COLLECTION ACTIONS:
+
+# index
+get '/contacts' do
+  @contacts = Contact.all
+  @contacts.map{|contact| contact.email}.to_s
+end
+
+# new
+get '/contacts/new' do
+  "This is the contacts new action"
+end
+
+# create
+post '/contacts' do
+  "This is the contacts create action"
+end
+
+
+# MEMBER ACTIONS:
+
+# show
+get '/contacts/:id' do
+  @contact = Contact.find(params[:id])
+  @contact.email
+end
+
+# edit
+get '/contacts/:id/edit' do
+  "This is the contacts edit action"
+end
+
+# update
+patch '/contacts/:id' do
+  "This is the contacts update action"
+end
+
+put '/contacts/:id' do
+  "This is the contacts update action"
+end
+
+# delete
+delete '/contacts/:id' do
+  "This is the contacts delete action"
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -8,7 +8,6 @@ new_contacts_needed.times do
   person = {first_name: Faker::Name.first_name, last_name: Faker::Name.last_name}
   person[:email] = Faker::Internet.safe_email("#{person[:first_name]}.#{person[:last_name]}")
 
-
   contact = Contact.new(person)
   contact.save!
 end
